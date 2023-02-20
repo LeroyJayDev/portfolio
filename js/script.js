eedb2615a4197ec6eac40718953f0c58de1cb0d4
Leroy Jay <123365396+LeroyJayDev@users.noreply.github.com>
2023-02-20 12:45:23 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ==== Typing Animation ==== */
+var typed = new Typed(".typing",{
+    strings:["","Web Designer", "Web Developer", "Content Writer", "Digital Marketer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
